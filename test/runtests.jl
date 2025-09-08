abb20e3377b098d28b9e555eaa87a7d7dabdb942
Daniel <Winkler.daniel1@gmail.com>
2025-09-08 19:37:25 +1000

    Refactor readcols calls to use macro syntax

--- test/runtests.jl
+++ test/runtests.jl
@@ -53,15 +53,15 @@ end
 end
 
 @testset "readcols" begin
-    webl = readcols("testfiles/jsonlwebsite.jsonl", :name) |> materialize  |> DataFrame
+    webl = @readcols("testfiles/jsonlwebsite.jsonl", 1, :name) |> materialize  |> DataFrame
     @test webl == full_web[:, [:name]]
-    mtl = readcols("testfiles/mtcars.jsonl", :gear, :hp; nworkers = 4) |> materialize |> DataFrame
+    mtl = @readcols("testfiles/mtcars.jsonl", 4, :gear, :hp) |> materialize |> DataFrame
     @test mtl == noprom_mtcars[:, [:gear, :hp]]
-    onel = readcols("testfiles/oneline.jsonl", :age) |> materialize |> DataFrame
+    onel = @readcols("testfiles/oneline.jsonl", 1, :age) |> materialize |> DataFrame
     @test onel == oneline[:, [:age]]
-    onepl = readcols("testfiles/oneline_plus.jsonl", :name) |> materialize |> DataFrame
+    onepl = @readcols("testfiles/oneline_plus.jsonl", 1, :name) |> materialize |> DataFrame
     @test onepl == oneline_plus[:, [:name]]
-    escl = readcols("testfiles/escapedeol.jsonl", :name) |> materialize |> DataFrame
+    escl = @readcols("testfiles/escapedeol.jsonl", 1, :name) |> materialize |> DataFrame
     @test escl == escaped[:, [:name]]
 end
 
